eacb8a77d8fb75bd235ff741d905d7a70a997371
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-11-09 18:48:40 +0330

    add fields participants table!

--- database/migrations/2022_11_09_150838_create_participants_table.php
+++ database/migrations/2022_11_09_150838_create_participants_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('participants', function (Blueprint $table) {
             $table->id();
+            $table->integer('game_id');
+            $table->string('tel',12);
+            $table->tinyInteger('team1_goal');
+            $table->tinyInteger('team2_goal');
+            $table->enum('win',['yes','no']);
             $table->timestamps();
         });
     }
