e5e44e5634f53d4d95f6e7848012dce6a542bfb2
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-11-09 19:11:35 +0330

    design category ui panel!

--- resources/views/lottory/addRace.blade.php
+++ resources/views/lottory/addRace.blade.php
@@ -1,26 +1,19 @@
 @extends('lottory.panel')
 @section('title')
-Add Race 🏆
+Categories 🔠
 @endsection
 @section('content')
     <form class="mt-6 space-y-8 divide-y divide-gray-200">
         <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
             <div class="space-y-6 pt-8 sm:space-y-5 sm:pt-10">
                 <div>
-                    <h3 class="text-lg font-medium leading-6 text-gray-900">Definition of competition</h3>
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">Definition of Category</h3>
                 </div>
                 <div class="space-y-6 sm:space-y-5">
                     <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                        <label for="first-team" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">First Team</label>
+                        <label for="category-name" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Category Name</label>
                         <div class="mt-1 sm:col-span-2 sm:mt-0">
-                            <input type="text" name="first-team" id="first-team" autocomplete="given-name" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm">
-                        </div>
-                    </div>
-
-                    <div class="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
-                        <label for="second-team" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">Second Team</label>
-                        <div class="mt-1 sm:col-span-2 sm:mt-0">
-                            <input type="text" name="second-team" id="second-team" autocomplete="second-team" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm">
+                            <input type="text" name="category-name" id="category-name" autocomplete="given-name" class="block w-full max-w-lg rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:max-w-xs sm:text-sm">
                         </div>
                     </div>
                 </div>
@@ -34,5 +27,38 @@ Add Race 🏆
             </div>
         </div>
     </form>
+
+    <div class="mt-12 px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-xl font-semibold text-gray-900">List Of Category</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all in lottery </p>
+            </div>
+        </div>
+        <div class="mt-8 flex flex-col">
+            <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Category Title</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Created At</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">Lindsay Walton</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Front-end Developer</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500"> <a href="#" class="text-red-600 hover:text-red-900">Edit</a></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 @endsection
