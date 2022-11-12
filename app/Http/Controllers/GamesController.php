0600509374858577297ef9be95bb7d10da9ff87d
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-11-12 20:50:19 +0330

    create index function & ui for define game !

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Game;
 use Illuminate\Http\Request;
 
 class GamesController extends Controller
 {
-    //
+    public function index()
+    {
+        $games = Game::all();
+        return view('lottery.game',compact('games'));
+    }
 }
