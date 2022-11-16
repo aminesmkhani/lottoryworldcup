04468841f546e9562c90e449470ad18ef9dbf821
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-11-16 21:13:01 +0330

    add relation for winner in category!

--- app/Http/Controllers/WinnerController.php
+++ app/Http/Controllers/WinnerController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Winner;
 use Illuminate\Http\Request;
 
 class WinnerController extends Controller
 {
     public function index()
     {
+        $winner = Winner::with('category')->get();
+        dd($winner);
         return view('lottery.winner');
     }
 }
