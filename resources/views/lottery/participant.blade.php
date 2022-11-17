c7f5261997bab72669224fdf46ef53b1aeaba7c6
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-11-17 12:14:18 +0330

    design basic ui for participants!

--- resources/views/lottery/participant.blade.php
+++ resources/views/lottery/participant.blade.php
@@ -1,13 +1,13 @@
 @extends('lottery.panel')
 @section('title')
-Winners 🏆
+Participants 👪
 @endsection
 @section('content')
     <div class="mt-12 px-4 sm:px-6 lg:px-8">
         <div class="sm:flex sm:items-center">
             <div class="sm:flex-auto">
-                <h1 class="text-xl font-semibold text-gray-900">List Of Winners</h1>
-                <p class="mt-2 text-sm text-gray-700">A list of all in winners </p>
+                <h1 class="text-xl font-semibold text-gray-900">List Of Participants</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all in participants </p>
             </div>
         </div>
         <div class="mt-8 flex flex-col">
@@ -20,32 +20,30 @@ Winners 🏆
                                 <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Game Title</th>
                                 <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Phone</th>
                                 <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Game Result</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Category</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Sms Verify</th>
-                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Actions</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
                             </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
-                            @foreach($winners as $winner)
-                            <tr>
-                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{$winner->game->team1}} - {{$winner->game->team2}}</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->tel}}</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->game->team1}} {{$winner->game->team1_goal}} - {{$winner->game->team2}} {{$winner->game->team2_goal}}</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->category->title}}</td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    @if($winner->sms_verify == "yes")
-                                        <span class="inline-flex items-center rounded-full bg-green-400
-                                         px-2.5 py-0.5 text-xs font-medium text-green-800">Sent</span>
-                                    @else
-                                        <span class="inline-flex items-center rounded-full bg-yellow-400
-                                         px-2.5 py-0.5 text-xs font-medium text-yellow-800">Not Sent</span>
-                                    @endif
-                                </td>
-                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                    <a href="{{route('sendWinnerSms',$winner->id)}}" class="text-blue-600 hover:text-blue-900">Send Sms</a>&nbsp;
-                                </td>
-                            </tr>
-                            @endforeach
+{{--                            @foreach($winners as $winner)--}}
+{{--                            <tr>--}}
+{{--                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">{{$winner->game->team1}} - {{$winner->game->team2}}</td>--}}
+{{--                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->tel}}</td>--}}
+{{--                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->game->team1}} {{$winner->game->team1_goal}} - {{$winner->game->team2}} {{$winner->game->team2_goal}}</td>--}}
+{{--                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{$winner->category->title}}</td>--}}
+{{--                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">--}}
+{{--                                    @if($winner->sms_verify == "yes")--}}
+{{--                                        <span class="inline-flex items-center rounded-full bg-green-400--}}
+{{--                                         px-2.5 py-0.5 text-xs font-medium text-green-800">Sent</span>--}}
+{{--                                    @else--}}
+{{--                                        <span class="inline-flex items-center rounded-full bg-yellow-400--}}
+{{--                                         px-2.5 py-0.5 text-xs font-medium text-yellow-800">Not Sent</span>--}}
+{{--                                    @endif--}}
+{{--                                </td>--}}
+{{--                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">--}}
+{{--                                    <a href="{{route('sendWinnerSms',$winner->id)}}" class="text-blue-600 hover:text-blue-900">Send Sms</a>&nbsp;--}}
+{{--                                </td>--}}
+{{--                            </tr>--}}
+{{--                            @endforeach--}}
                             </tbody>
                         </table>
                     </div>
